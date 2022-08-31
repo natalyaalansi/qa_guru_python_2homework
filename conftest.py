7a693340f1b450fe417b1ab321a353283c32928c
NatalyaAlansi <alansinatalya@gmail.com>
2022-08-31 20:54:48 +0400

    1)Put Pytest fixtures in conftest

--- conftest.py
+++ conftest.py
@@ -5,3 +5,12 @@ from selene.support.shared import browser
 def open_browser():
     browser.open('https://www.google.com/ncr')
     yield
+
+@pytest.fixture()
+def configure_window_size():
+    browser.config.window_width = 640
+    browser.config.window_height = 480
+
+@pytest.fixture()
+def configure_max_window_size():
+    browser.driver.maximize_window()
